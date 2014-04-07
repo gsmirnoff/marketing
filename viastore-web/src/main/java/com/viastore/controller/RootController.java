24fa009c335deb8eaa128b2eb269655281240661
Greg Smirnoff <gregsm1988@gmail.com>
2014-04-07 18:49:13 +0400

    added json support

--- viastore-web/src/main/java/com/viastore/controller/RootController.java
+++ viastore-web/src/main/java/com/viastore/controller/RootController.java
@@ -1,10 +1,13 @@
 package com.viastore.controller;
 
+import com.viastore.response.ResponseEntity;
 import org.springframework.stereotype.Component;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 /**
  * Created with IntelliJ IDEA.
@@ -18,7 +21,9 @@ import javax.ws.rs.core.Response;
 public class RootController {
 
     @GET
-    public Response index() {
-        return Response.status(200).entity("I'm working").build();
+    @Produces({MediaType.APPLICATION_JSON})
+    @Consumes({MediaType.APPLICATION_JSON})
+    public ResponseEntity index() {
+        return new ResponseEntity(0, null);
     }
 }
---
+++ viastore-web/src/main/java/com/viastore/response/ResponseEntity.java
@@ -0,0 +1,32 @@
+package com.viastore.response;
+
+/**
+ * Created by GSmirnoff on 07.04.14.
+ */
+public class ResponseEntity {
+    private int status;
+    private Object response;
+
+    public ResponseEntity() {}
+
+    public ResponseEntity(int status, Object response) {
+        this.status = status;
+        this.response = response;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public Object getResponse() {
+        return response;
+    }
+
+    public void setResponse(Object response) {
+        this.response = response;
+    }
+}
