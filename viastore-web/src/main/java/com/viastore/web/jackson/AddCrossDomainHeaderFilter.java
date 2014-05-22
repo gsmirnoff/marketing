8870f8a4bd616ec2de4fa172b1a2ebe98c2e67ac
Greg Smirnoff <gregsm1988@gmail.com>
2014-05-22 18:41:59 +0400

    added cross-domain header

---
+++ viastore-web/src/main/java/com/viastore/web/jackson/AddCrossDomainHeaderFilter.java
@@ -0,0 +1,19 @@
+package com.viastore.web.jackson;
+
+import com.sun.jersey.spi.container.ContainerRequest;
+import com.sun.jersey.spi.container.ContainerResponse;
+import com.sun.jersey.spi.container.ContainerResponseFilter;
+
+import javax.ws.rs.ext.Provider;
+
+/**
+ * Created by GSmirnoff on 22.05.14.
+ */
+@Provider
+public class AddCrossDomainHeaderFilter implements ContainerResponseFilter {
+    @Override
+    public ContainerResponse filter(ContainerRequest request, ContainerResponse containerResponse) {
+        containerResponse.getHttpHeaders().add("Access-Control-Allow-Origin", "*");
+        return containerResponse;
+    }
+}
